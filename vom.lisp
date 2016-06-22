278f1bba3b9a1bb9a045f9eb8002093cca9542d6
Andrew Danger Lyon <orthecreedence@gmail.com>
2016-06-22 15:40:42 -0700

    caching some lookups and rearranging a few calls to (hopefully) give a sizeable performance boost

--- vom.lisp
+++ vom.lisp
@@ -10,6 +10,7 @@
                 #:eval-when
                 #:eval
                 #:lambda #:defun #:multiple-value-list #:defmacro
+                #:return-from
                 #:defvar #:defparameter
                 #:declare #:optimize #:type #:ignore
                 #:keyword #:integer
@@ -84,6 +85,9 @@
    log-level for that package, returns one or more (via (values ...)) streams
    that this log will be sent to.")
 
+(defvar *package-level-cache* nil
+  "A cache that holds package alias -> package loglevel values for quick lookup.")
+
 (defparameter *time-formatter*
   (lambda ()
     (multiple-value-bind (second minute hour)
@@ -114,6 +118,7 @@
   "Configure the log level for a package (or use t for the package name to set
    the default log level). The log level is given as a keyword."
   (assert (member level-name *levels*))
+  (clear-level-cache)
   (cond ((eq package-keyword t)
          (setf (getf *config* t) level-name))
         ((symbolp package-keyword)
@@ -123,6 +128,30 @@
                                           package-keyword))))
            (setf (getf *config* (intern package-name :keyword)) level-name)))))
 
+(defun find-package-level (package-keyword)
+  "Given  package keyword (doesn't have to be an exact match, can be an alias),
+   find the configured loglevel of that package.
+   
+   This caches the package->level connection in *package-level-cache*."
+  (declare (optimize (cl:speed 3) (cl:safety 0) (cl:debug 0))
+           (type keyworkd package-keyword))
+  (let ((cached (getf *package-level-cache* package-keyword)))
+    (when cached (return-from find-package-level cached))
+    (let* ((package (find-package package-keyword))
+           (package-name (when package
+                           (intern (package-name package) :keyword)))
+           (package-level (getf *config* package-name))
+           (package-level (if package-level
+                              package-level
+                              (getf *config* t)))
+           (package-level-value (getf *levels* package-level 0)))
+      (setf (getf *package-level-cache* package-keyword) package-level-value)
+      package-level-value)))
+
+(defun clear-level-cache ()
+  "Clears the package loglevel cache."
+  (setf *package-level-cache* nil))
+  
 (defun do-log (level-name log-level package-keyword format-str &rest args)
   "The given data to the current *log-stream* stream."
   (declare (optimize (cl:speed 3) (cl:safety 0) (cl:debug 0))
@@ -130,22 +159,15 @@
            (type integer log-level)
            (type string format-str)
            (type list args))
-  (let* ((package (find-package package-keyword))
-         (package-name (when package
-                         (intern (package-name package) :keyword)))
-         (package-level (getf *config* package-name))
-         (package-level (if package-level
-                            package-level
-                            (getf *config* t)))
-         (package-level-value (getf *levels* package-level 0))
-         (log-streams (multiple-value-list
-                        (funcall *log-hook*
-                                 log-level
-                                 package-keyword
-                                 package-level-value))))
+  (let* ((package-level-value (find-package-level package-keyword)))
     (when (<= log-level package-level-value)
       (let* ((level-str (string level-name))
-             (logline (funcall *log-formatter* format-str level-str package-keyword args)))
+             (logline (funcall *log-formatter* format-str level-str package-keyword args))
+             (log-streams (multiple-value-list
+                            (funcall *log-hook*
+                                     log-level
+                                     package-keyword
+                                     package-level-value))))
         (dolist (stream log-streams)
           (write-sequence logline (if (eq stream t)
                                       cl:*standard-output*
